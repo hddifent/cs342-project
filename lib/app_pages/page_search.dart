e79261382c5d0f67ebae924d1099e6e9ad72a995
hddifent <preukwork@gmail.com>
2024-03-07 02:59:49 +0700

    Modified Search page to add search bar and mockup DormCard.

--- lib/app_pages/page_search.dart
+++ lib/app_pages/page_search.dart
@@ -1,3 +1,5 @@
+import "package:cs342_project/models/dorm.dart";
+import "package:cs342_project/widgets/dorm_card.dart";
 import "package:flutter/material.dart";
 
 class SearchPage extends StatelessWidget {
@@ -5,6 +7,27 @@ class SearchPage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Search Page (In progress...)"));
+    return Padding(
+      padding: const EdgeInsets.all(10),
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: const SearchBar(
+              leading: Icon(Icons.search),
+              hintText: "Search Dorm..."
+            ),
+          ),
+          
+          const SizedBox(height: 10),
+
+          const DormCard(
+            dorm: Dorm(dormName: "KU Home", dormDescription: "Right there, inside the university. You can't get any better than this."),
+          )
+        ]
+      ),
+    );
   }
 }
