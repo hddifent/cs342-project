7d52c0d933f0185a3794e8dc479944e066fe890c
OatAotOat <oat.engineer1987@gmail.com>
2024-03-27 22:25:52 +0700

    Initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import '../app_pages/page_log_in.dart';
 import 'constants.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  initializeFirebase();
   runApp(const MyApp());
+}
+
+void initializeFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
 }
 
 class MyApp extends StatelessWidget {
