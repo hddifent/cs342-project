910a40991bebd01983e9c472ed43069b8833994f
hddifent <preukwork@gmail.com>
2024-04-01 16:50:49 +0700

    Dorms can now be sorted. (Except for by Ratings)

--- lib/app_pages/page_search.dart
+++ lib/app_pages/page_search.dart
@@ -1,9 +1,13 @@
+import "dart:math";
+
 import "package:cloud_firestore/cloud_firestore.dart";
 import "package:cs342_project/constants.dart";
 import "package:cs342_project/database/firestore.dart";
 import "package:cs342_project/models/dorm.dart";
+import "package:cs342_project/utils/geolocator_locate.dart";
 import "package:cs342_project/widgets/dorm_card.dart";
 import "package:flutter/material.dart";
+import "package:google_maps_flutter/google_maps_flutter.dart";
 
 import "../widgets/category_button.dart";
 
@@ -18,13 +22,21 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   final _searchController = TextEditingController();
 
-  int current = 0;
+  bool _loading = true;
+  LatLng _userLocation = const LatLng(0, 0);
 
-  List<String> categorys = ['A - Z', 'Nearest', 'Rating', 'Price'];
+  @override
+  void initState() {
+    super.initState();
+    _getUserLocation();
+  }
+
+  int sortType = 0;
+  List<String> categories = ['A - Z', 'Nearest', 'Rating', 'Price'];
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return _loading ? const Center(child: Text("Loading Data...")) : Padding(
       padding: const EdgeInsets.all(10),
 
       child: Column(
@@ -35,18 +47,18 @@ class _SearchPageState extends State<SearchPage> {
             child: SearchBar(
               leading: const Icon(Icons.search),
               hintText: "Search Dorm...",
-              controller: _searchController,
-            ),
+              controller: _searchController
+            )
           ),
 
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 5),
-            child: _categoryTabBar(),
+            child: _categoryTabBar()
           ),
 
-          _dormList()
+          _dormList(sortType)
         ]
-      ),
+      )
     );
   }
 
@@ -57,39 +69,72 @@ class _SearchPageState extends State<SearchPage> {
         scrollDirection: Axis.horizontal,
         clipBehavior: Clip.antiAliasWithSaveLayer,
         padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2.5),
-        itemBuilder: (context, index) => 
-          GestureDetector(
-            onTap: () async {
-              if (current != index) {
-                setState(() {
-                  current = index;
-                });
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              if (sortType != index) {
+                setState(() => sortType = index);
               }
             },
             child: CategoryButton(
-              category: categorys[index],
-              backgroundColor: current == index ? AppPalette.green : Colors.white,
-            ),
-          ),
-        itemCount: categorys.length,
-      ),
+              category: categories[index],
+              backgroundColor: sortType == index ? AppPalette.green : Colors.white,
+            )
+          );
+        },
+        itemCount: categories.length,
+      )
     );
   }
 
-  Widget _dormList() {
+  Widget _dormList(int sortType) {
     FirestoreDatabase db = FirestoreDatabase("dorms");
     return StreamBuilder<QuerySnapshot>(
       stream: db.getStream("name"),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           List<QueryDocumentSnapshot<Object?>> dormDocList = snapshot.data?.docs ?? [];
+          List<Dorm> dormList = [];
           List<Widget> dormCardList = [];
 
           for (QueryDocumentSnapshot<Object?> doc in dormDocList) {
             Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
             Dorm dorm = Dorm.fromFirestore(data);
+            dormList.add(dorm);
+          }
 
-            dormCardList.add(DormCard(dorm: dorm));
+          if (sortType == 0) {
+            dormList.sort((a, b) => a.dormName.compareTo(b.dormName));
+          }
+          else if (sortType == 1) {
+            Map<int, double> distanceIndexMap = { };
+            Point userPoint = Point(_userLocation.latitude, _userLocation.longitude);
+
+            for (int i = 0; i < dormList.length; i++) {
+              Dorm d = dormList[i];
+              Point dormPoint = Point(d.geoLocation.latitude, d.geoLocation.longitude);
+              distanceIndexMap.addAll({i: userPoint.distanceTo(dormPoint)});
+            }
+
+            List<MapEntry<int, double>> sortedEntries = distanceIndexMap.entries.toList()..sort((a, b) => a.value.compareTo(b.value));
+            distanceIndexMap..clear()..addEntries(sortedEntries);
+            List<Dorm> sorted = [];
+
+            for (int i = 0; i < distanceIndexMap.length; i++) {
+              sorted.add(dormList[distanceIndexMap.keys.toList()[i]]);
+            }
+
+            dormList = List.from(sorted);
+          }
+          else if (sortType == 3) {
+            dormList.sort((a, b) => a.monthlyPrice.compareTo(b.monthlyPrice));
+          }
+          else {
+            dormList.sort((a, b) => a.dormName.compareTo(b.dormName));
+          }
+
+          for (Dorm d in dormList) {
+            dormCardList.add(DormCard(dorm: d));
           }
 
           return Expanded(
@@ -101,7 +146,15 @@ class _SearchPageState extends State<SearchPage> {
         else {
           return const Text("No Dorm in database...");
         }
-      },
+      }
     );
+  }
+
+  void _getUserLocation() async {
+    LatLng uLoc = await UserLocator.getUserLocation();
+    setState(() {
+      _userLocation = uLoc;
+      _loading = false;
+    });
   }
 }
