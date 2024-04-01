5821e91c6dd8c774f77151df648aa5d355b94b1b
hddifent <preukwork@gmail.com>
2024-04-01 23:15:02 +0700

    Markers are now displayed on the map.

--- lib/app_pages/page_discovery.dart
+++ lib/app_pages/page_discovery.dart
@@ -1,3 +1,6 @@
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:cs342_project/database/firestore.dart";
+import "package:cs342_project/models/dorm.dart";
 import "package:cs342_project/utils/geolocator_locate.dart";
 import "package:flutter/material.dart";
 import "package:google_maps_flutter/google_maps_flutter.dart";
@@ -12,32 +15,64 @@ class DiscoveryPage extends StatefulWidget {
 class _DiscoveryPageState extends State<DiscoveryPage> {
   bool _loading = true;
   LatLng _userLocation = const LatLng(0, 0);
+  Set<Marker> _dormMarkers = { };
 
   @override
   void initState() {
     super.initState();
-    _getUserLocation();
+    _getInitData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: _loading ? const Text("Loading Map...") : GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: CameraPosition(
-          target: _userLocation,
-          zoom: 12.5
-        )
+      child: _loading ? const Text("Loading Map...") : Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: CameraPosition(
+              target: _userLocation,
+              zoom: 12.5
+            ),
+            markers: _dormMarkers,
+          ),
+
+          
+        ]
       )
     );
   }
 
-  void _getUserLocation() async {
-    LatLng uLoc = await UserLocator.getUserLocation();
+  void _getInitData() async {
+    await Future.wait([
+      UserLocator.getUserLocation().then((value) => _userLocation = value),
+      _getMarkersFromDB().then((value) => _dormMarkers = value)
+    ]);
+
     if (!mounted) { return; }
-    setState(() {
-      _userLocation = uLoc;
-      _loading = false;
+    setState(() => _loading = false);
+  }
+
+  Future<Set<Marker>> _getMarkersFromDB() async {
+    Set<Marker> markers = { };
+
+    FirestoreDatabase db = FirestoreDatabase("dorms");
+    db.collection.get().then((dormCollection) {
+      if (dormCollection.docs.isNotEmpty) {
+        for (QueryDocumentSnapshot<Object?> doc in dormCollection.docs) {
+          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+          Dorm dorm = Dorm.fromFirestore(data);
+
+          markers.add(
+            Marker(
+              markerId: MarkerId(doc.id),
+              position: LatLng(dorm.geoLocation.latitude, dorm.geoLocation.longitude)
+            )
+          );
+        }
+      }
     });
+
+    return markers;
   }
 }
