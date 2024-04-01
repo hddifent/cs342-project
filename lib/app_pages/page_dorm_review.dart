e229fce30ae8efbdcf2d781616d46aee2c71ffcb
hddifent <preukwork@gmail.com>
2024-04-02 05:11:14 +0700

    Hide write reviews function from guests.

--- lib/app_pages/page_dorm_review.dart
+++ lib/app_pages/page_dorm_review.dart
@@ -1,5 +1,6 @@
 import 'package:cs342_project/app_pages/page_write_review.dart';
 import 'package:cs342_project/constants.dart';
+import 'package:cs342_project/global.dart';
 import 'package:cs342_project/models/dorm.dart';
 import 'package:cs342_project/widgets/green_button.dart';
 import 'package:flutter/material.dart';
@@ -19,27 +20,32 @@ class _DormReviewPageState extends State<DormReviewPage> {
     return Padding(
       padding: const EdgeInsets.all(10),
       child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-
-          children: <Widget>[
-            // TODO: Check for user review...
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Your Review", 
-                  style: AppTextStyle.heading1.merge(AppTextStyle.bold)
-                ), 
-                greenButton("Write", _writeReview, width: 100, icon: Icons.edit)
-              ]
-            ),
-
-            const SizedBox(height: 10),
-
-            Text("Reviews (???)", style: AppTextStyle.heading1.merge(AppTextStyle.bold))
-
-            // TODO: Check for all reviews...
-          ]
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            
+          
+            children: <Widget>[
+              // TODO: Check for user review...
+              currentUser == null ? const SizedBox(height: 0) : Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Your Review",
+                    style: AppTextStyle.heading1.merge(AppTextStyle.bold)
+                  ),
+                  greenButton("Write", _writeReview, width: 100, icon: Icons.edit)
+                ]
+              ),
+          
+              currentUser == null ? const SizedBox(height: 0) : const SizedBox(height: 10),
+          
+              Text("Reviews (???)", style: AppTextStyle.heading1.merge(AppTextStyle.bold))
+          
+              // TODO: Check for all reviews...
+            ]
+          ),
         )
       )
     );
